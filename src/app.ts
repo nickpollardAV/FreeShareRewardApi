187656837cc28c6cc62cf48327fc73a0f61c3427
nickpollardAV <nickpollard23@hotmail.com>
2022-05-25 13:33:15 +0100

    backup commit 2

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ console.log("NEW LOG 2")
 console.log("NEW LOG 3")
 console.log("NEW LOG 4")
 console.log("NEW LOG 5")
+console.log("NEW LOG 6")
 
 // Use body parser to read sent json payloads
 app.use(
